4189bb8a0ca088e7a5f6af4ffa8deebb1f6cfce0
Daniel F Moisset <dmoisset@machinalis.com>
2016-08-26 14:52:19 +0100

    Added annotation for view

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello, world. You're at the polls index.")
