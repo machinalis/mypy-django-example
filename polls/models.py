74335dd926879bb68ab411f48c34ed3f330cd01b
Daniel F Moisset <dmoisset@machinalis.com>
2016-09-01 18:48:19 +0100

    Add annotations for __str__ methods

--- polls/models.py
+++ polls/models.py
@@ -5,7 +5,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.question_text
 
 
@@ -14,6 +14,6 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.question_text
 
