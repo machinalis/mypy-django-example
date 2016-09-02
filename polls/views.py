60c1ff46f6aafe678781f87711b66e481110c26c
Daniel F Moisset <dmoisset@machinalis.com>
2016-09-02 12:35:53 +0100

    Added annotations for views

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,15 @@
 from django.http import HttpRequest, HttpResponse
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello, world. You're at the polls index.")
 
-def detail(request, question_id):
+def detail(request: HttpRequest, question_id: str) -> HttpResponse:
     return HttpResponse("You're looking at question %s." % question_id)
 
-def results(request, question_id):
+def results(request: HttpRequest, question_id: str) -> HttpResponse:
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
-def vote(request, question_id):
+def vote(request: HttpRequest, question_id: str) -> HttpResponse:
     return HttpResponse("You're voting on question %s." % question_id)
