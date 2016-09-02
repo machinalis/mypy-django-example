e0140846dab136b04b948e57eba024de63ab5699
Daniel F Moisset <dmoisset@machinalis.com>
2016-09-02 15:34:11 +0100

    Improve admin look

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin  # type: ignore
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
