9d1467c321b8a4207752d9bce224876845e5084e
Daniel F Moisset <dmoisset@machinalis.com>
2016-09-02 12:44:06 +0100

    Add logic for the poll list

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpRequest, HttpResponse
 
+from .models import Question
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request: HttpRequest, question_id: str) -> HttpResponse:
     return HttpResponse("You're looking at question %s." % question_id)
