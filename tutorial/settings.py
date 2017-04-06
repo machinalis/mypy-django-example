d81b272a0124d73de4e7eef7c0785bb48d62697f
Daniel F Moisset <dmoisset@machinalis.com>
2017-04-06 11:31:31 +0200

    Add missing import

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 import os
+from typing import List
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
